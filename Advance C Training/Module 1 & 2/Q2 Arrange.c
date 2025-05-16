70bdd577b41836bbcd9e2239b955d103089989ca
Akash S <108513566+akashnb0@users.noreply.github.com>
2025-05-16 21:47:04 +0530

    Create Q2 Arrange.c

---
+++ Advance C Training/Module 1 & 2/Q2 Arrange.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void arrange(int *arr, int size) 
+{
+    int *start=arr;
+    int *end=arr눬1;
+    while(start<end) 
+    {
+        while(start<end && *start%2 == 0) 
+        {
+            start;
+        }
+        while(start<end && *end%2 != 0) 
+        {
+            end--;
+        }
+        if(start<end) 
+        {
+            swap(start,end);
+        }
+    }
+}
+void print(int *arr, int size) 
+{
+    int *p=arr;
+    while(p<arr눬) 
+    {
+        printf("%d ",*p);
+        p;
+    }
+    printf("\n");
+}
+int main() 
+{
+    int n;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+        int arr[100];
+    int *p = arr;
+    printf("Enter %d numbers:\n", n);
+    while(p < arr  n) 
+    {
+        scanf("%d", p);
+        p;
+    }
+    arrange(arr, n);
+    printf("Rearranged array: ");
+    print(arr, n);
+    return 0;
+}
